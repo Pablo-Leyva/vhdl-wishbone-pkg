25275d4b59f8dc80f6b6134369ed05ef6057d893
Pablo Leyva <pablo.leyva@huawei.com>
2020-06-15 14:32:33 +0200

    Added new_wishbone3 function for interface init

--- pkg/wishbone3_utils_pkg.vhd
+++ pkg/wishbone3_utils_pkg.vhd
@@ -11,6 +11,12 @@ package wishbone3_utils_pkg is
     type wishbone3_m2s_array_t is array (natural range <>) of wishbone3_m2s_t;
     type wishbone3_s2m_array_t is array (natural range <>) of wishbone3_s2m_t;
 
+    pure function new_wishbone_m2s( i : wishbone3_interface_cnf_t ) return wishbone3_m2s_t;
+    pure function new_wishbone_s2m( i : wishbone3_interface_cnf_t ) return wishbone3_s2m_t;
+
+    pure function new_wishbone_m2s_array( i : wishbone3_interface_cnf_t; n : positive ) return wishbone3_m2s_array_t;
+    pure function new_wishbone_s2m_array( i : wishbone3_interface_cnf_t; n : positive ) return wishbone3_s2m_array_t;
+
     pure function get_data_width( i : wishbone3_m2s_t )           return natural;
     pure function get_data_width( i : wishbone3_s2m_t )           return natural;
     pure function get_data_width( i : wishbone3_interface_t )     return natural;
@@ -34,6 +40,50 @@ package wishbone3_utils_pkg is
 end wishbone3_utils_pkg;
 
 package body wishbone3_utils_pkg is
+
+    pure function new_wishbone_m2s( i : wishbone3_interface_cnf_t ) return wishbone3_m2s_t is
+        variable wishbone3_m2s_v : wishbone3_m2s_t(addr(get_addr_width(i)-1 downto 0),
+                                                   data(get_data_width(i)-1 downto 0),
+                                                    sel(get_sel_width(i) -1 downto 0));
+    begin
+        wishbone3_m2s_v.addr := (others => '0');
+        wishbone3_m2s_v.data := (others => '0');
+        wishbone3_m2s_v.sel  := (others => '0');
+        wishbone3_m2s_v.we   := '0';
+        wishbone3_m2s_v.stb  := '0';
+        wishbone3_m2s_v.cyc  := '0';
+        return wishbone3_m2s_v;
+    end;
+
+    pure function new_wishbone_s2m( i : wishbone3_interface_cnf_t ) return wishbone3_s2m_t is
+        variable wishbone3_s2m_v : wishbone3_s2m_t(data(get_data_width(i)-1 downto 0));
+    begin
+        wishbone3_s2m_v.data := (others => '0');
+        wishbone3_s2m_v.ack  := '0';
+        wishbone3_s2m_v.err  := '0';
+        wishbone3_s2m_v.rty  := '0';
+        return wishbone3_s2m_v;
+    end;
+
+    pure function new_wishbone_m2s_array(i : wishbone3_interface_cnf_t; n : positive) return wishbone3_m2s_array_t is
+        variable wishbone3_m2s_array_v : wishbone3_m2s_array_t(0 to n-1)(addr(get_addr_width(i)-1 downto 0),
+                                                                         data(get_data_width(i)-1 downto 0),
+                                                                          sel(get_sel_width(i) -1 downto 0));
+    begin
+        for j in 0 to n-1 loop
+            wishbone3_m2s_array_v(j) := new_wishbone_m2s(i);
+         end loop;
+        return wishbone3_m2s_array_v;
+    end;
+
+    pure function new_wishbone_s2m_array(i : wishbone3_interface_cnf_t; n : positive) return wishbone3_s2m_array_t is
+        variable wishbone3_s2m_array_v : wishbone3_s2m_array_t(0 to n-1)(data(get_data_width(i)-1 downto 0));
+    begin
+        for j in 0 to n-1 loop
+            wishbone3_s2m_array_v(j) := new_wishbone_s2m(i);
+         end loop;
+        return wishbone3_s2m_array_v;
+    end;
 
     pure function get_data_width( i : wishbone3_m2s_t ) return natural is begin
         return i.data'length;
